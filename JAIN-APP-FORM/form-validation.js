37ef8b1e5870ed5e05b7f77b3c55182ef1ed2e8c
vileroze <50352881+vileroze@users.noreply.github.com>
2023-01-09 19:21:05 +0545

    adding new user works

--- JAIN-APP-FORM/form-validation.js
+++ JAIN-APP-FORM/form-validation.js
@@ -1,3 +1,32 @@
-import { sayHello } from "./firebase-functions.js";
+import { writeUserData } from "./firebase-functions.js";
 
-document.getElementById("send_info").addEventListener("click", sayHello);
+// Function to get get form values
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+// Listen for form submit
+document.getElementById('signup_form').addEventListener('submit', submitForm);
+
+// action to be performed after form submission
+function submitForm(e){
+    e.preventDefault();
+    console.log("form-submission");
+
+    // Get values
+    let userFirstName = getInputVal('first_name');
+    let userLastName = getInputVal('last_name');
+    let userEmail = getInputVal('user_email');
+    let userPhone = getInputVal('user_phone');
+
+    // write user data to database
+    writeUserData(userPhone, userFirstName, userLastName, userEmail, "no");
+
+    // Show alert
+    alert("Record inserted");
+
+    // Clear form
+    document.getElementById('signup_form').reset();
+}
+
+
